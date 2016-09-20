24b155662ea410c393e319a399d2798766e07465
v.shamarinov <v.shamarinov@cbonds.info>
2016-09-20 17:57:38 +0300

    m0

--- application/controller/controller.php
+++ application/controller/controller.php
@@ -1,22 +1,10 @@
 <?php
 /**
- *
+ * test git
  */
 
 class CbProfilesManager
 {
-    /**
-     * Òåêóùèé ïğîôèëü - âûñòàâëÿåòñÿ â êîíñòğóêòîğå èíèöèàëèçèğóşùèì ïğèëîæåíèåì
-     * @var string
-     */
-    private $defaultProfileSid;
-
-    /**
-     * Íàñòğîéêè ãğóïï ïğîôèëåé [groupName => [profiles => [], allow => [], disallow => []], ...]
-     *
-     * @var array
-     */
-#if settings
     private static $settings = array(
         'project1'=>array(),
         'global' => array(
@@ -34,128 +22,6 @@ class CbProfilesManager
                 'MAIN_CB_GLOBAL_RUS',
                 'MAIN_CB_GLOBAL_DEU',
                 'MAIN_CB_GLOBAL_ESP',
-            ),
-        ),
-        'pl' => array(
-            'entities' => array('emission', 'emitents'),
-            'profiles'  => array(
-                'DEV_CB_POLAND_ENG',
-                'DEV_CB_POLAND_POL',
-                'DEV_CB_POLAND_RUS',
-                'MAIN_PL_CBONDS_INFO_ENG',
-                'MAIN_PL_CBONDS_INFO_RUS',
-                'MAIN_CBONDS_PL_PROFILE',
-            ),
-            'allow'     => array(
-                //'country.id'    => 24,
-                'emission_show_pl' => 1,
-            ),
-        ),
-        'ru' =>
-            array(
-                'entities' => array('emission', 'emitents'),
-                'profiles'  => array(
-                    'DEV_CB_RU_ENG',
-                    'DEV_CB_RU_RUS',
-                    'MAIN_CB_RU_ENG',
-                    'MAIN_CB_RU_RUS',
-                ),
-                'allow'     => array(
-                    'emission_show_ru' => 1,
-                ),
-            ),
-        'ua' => array(
-            'entities' => array('emission', 'emitents'),
-            'profiles'  => array(
-                'DEV_CB_UA_ENG',
-                'DEV_CB_UA_RUS',
-                'MAIN_CB_UA_COM',
-                'MAIN_CB_UA_RUS',
-            ),
-            'allow'     => array(
-                //'country.id'    => 72,
-                'emission_show_ua' => 1,
-            ),
-        ),
-        'em' => array(
-            'entities' => array('emission', 'emitents'),
-            'profiles'  => array(
-                'DEV_CB_EM_ENG',
-                'DEV_CB_EM_POL',
-                'DEV_CB_EM_RUS',
-                'DEV_CB_EM_ITA',
-                'DEV_CB_EM_DEU',
-                'DEV_CB_EM_ESP',
-                'MAIN_CB_EM_ENG',
-                'MAIN_CB_EM_RUS',
-                'MAIN_CB_EM_POL',
-                'MAIN_CB_EM_ITA',
-                'MAIN_CB_EM_DEU',
-                'MAIN_CB_EM_ESP',
-            ),
-            'allow'     => array(
-                //'kind.kind_id' => 2,
-                'emission_show_em' => 1,
-            ),
-            'disallow'  => array(
-                //'subregion.id'  => array(7, 9, 11),
-                //'kind.kind_id' => 1,
-            ),
-        ),
-        'default' => array(
-            'entities' => array('emission', 'emitents'),
-            'profiles'  => array(
-                'DEV_CBONDS_ENG',
-                'DEV_CBONDS_RUS',
-                'CBONDS_ENG',
-                'CBONDS_RUS',
-            ),
-        ),
-        'loans' => array(
-            'entities' => array('loan'),
-            'profiles' => array(
-                'DEV_LOANS_ENG',
-                'DEV_LOANS_RUS',
-                'LOANS_ENG',
-                'LOANS_RUS',
-            ),
-        ),
-        'old-loans' => array(
-            'entities' => array('loan'),
-            'profiles' => array(
-                'DEV_OLD_LOANS_ENG',
-                'DEV_OLD_LOANS_RUS',
-                'OLD_LOANS_ENG',
-                'OLD_LOANS_RUS',
-            ),
-        ),
-        'old-mm' => array(
-            'entities' => array(),
-            'profiles' => array(
-                'DEV_OLD_MONEY_MARKET',
-                'OLD_MONEY_MARKET',
-            ),
-        ),
-        'mm' => array(
-            'entities' => array(),
-            'profiles' => array(
-                'DEV_MONEY_MARKET_RUS',
-                'DEV_MONEY_MARKET_ENG',
-                'MONEY_MARKET_RUS',
-                'MONEY_MARKET_ENG',
-            ),
-        ),
-        'portfolio' => array(
-            'entities' => array(),
-            'profiles' => array(
-                'DEV_PORTFOLIO_CBONDS_INFO_ENG',
-                'DEV_PORTFOLIO_CBONDS_INFO_ITA',
-                'DEV_PORTFOLIO_CBONDS_INFO_POL',
-                'DEV_PORTFOLIO_CBONDS_INFO_RUS',
-                'RELEASE_PORTFOLIO_CBONDS_INFO_ENG',
-                'RELEASE_PORTFOLIO_CBONDS_INFO_RUS',
-                'RELEASE_PORTFOLIO_CBONDS_INFO_ITA',
-                'RELEASE_PORTFOLIO_CBONDS_INFO_POL',
             ),
         ),
         'test1' => array(),
